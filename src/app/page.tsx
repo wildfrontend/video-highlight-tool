1eddaf6cfbf8e1b6200894def1e67476bd17cea6
Louis Luo <>
2025-08-14 16:43:32 +0800

    Add @mui/icons-material dependency and update page layout; remove unused SVG files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import { Container } from '@mui/material';
+
+import UploadVideo from '@/components/upload/form';
+
 const Page = () => {
-  return <div>Hello</div>;
+  return (
+    <Container maxWidth="lg" sx={{ py: 4 }}>
+      <UploadVideo />
+    </Container>
+  );
 };
 
 export default Page;
